1048a845a955a0255f56ba27878157106b055830
Secret <k0haku@hotmail.com>
2019-05-06 15:16:59 +0200

    Added scrollbar to add notes

--- k0haku-notes/frames/add_notes.py
+++ k0haku-notes/frames/add_notes.py
@@ -2,6 +2,7 @@ from tkinter import ttk
 import tkinter as tk
 from tkinter import W, E, N, S
 
+from utils.helpers import AutoScrollbar
 
 class AddNotesWindowController:
     def __init__(self, view, controller):
@@ -27,33 +28,60 @@ class AddNotesWindow(tk.Frame):
         self.close_callback = callback
 
     def initUI(self):
-        self.columnconfigure(1, weight=1)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(5, pad=7)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        self.lbl = ttk.Label(self, text='Input:')
-        self.lbl.grid(sticky=W, pady=4, padx=5)
+        self.scrollbar = AutoScrollbar(self)
+        self.scrollbar.grid(row=0, column=1, sticky='ns')
 
-        self.txtarea = tk.Text(self)
-        self.txtarea.insert('1.0', self.note_txt)
-        self.txtarea.grid(row=1, column=0, columnspan=2,
-                          rowspan=4, padx=5, sticky=E+W+S+N)
+        self.canvas = tk.Canvas(self, yscrollcommand=self.scrollbar.set)
+        self.canvas.grid(row=0, column=0, sticky='nsew')
 
-        self.grid_rowconfigure(5, weight=1)
-        # self.grid_columnconfigure(0, weight=1)
-        self.buttons_down = tk.Frame(self)
-        self.buttons_down.grid(row=5, column=0, columnspan=2, sticky=N+E+S+W)
+        self.scrollbar.config(command=self.canvas.yview)
+
+        self.canvased_frame = tk.Frame(self.canvas)
+        self.canvased_frame.grid_rowconfigure(1, weight=1)
+        self.canvased_frame.grid_columnconfigure(1, weight=1)
+
+        self.time_lbl = ttk.Label(self.canvased_frame, text='Time:')
+        self.time_lbl.pack(side='top', fill='both', expand=True)
+    
+        self.type_lbl = ttk.Label(self.canvased_frame, text='Type:')
+        self.type_lbl.pack(side='top', fill='both', expand=True)
+
+        self.tags_lbl = ttk.Label(self.canvased_frame, text='Tags:')
+        self.tags_lbl.pack(side='top', fill='both', expand=True)
+
+        self.content_lbl = ttk.Label(self.canvased_frame, text='Content:')
+        self.content_lbl.pack(side='top', fill='both', expand=True)
+
+        self.content_text = tk.Text(self.canvased_frame)
+        self.content_text.insert('1.0', self.note_txt)
+        self.content_text.pack(side='top', fill='both', expand=True)
+
+        self.comment_lbl = ttk.Label(self.canvased_frame, text='Comment:')
+        self.comment_lbl.pack(side='top', fill='both', expand=True)
+
+        self.comment_text = tk.Text(self.canvased_frame)
+        self.comment_text.insert('1.0', self.note_txt)
+        self.comment_text.pack(side='top', fill='both', expand=True)
+
+        self.buttons_down = tk.Frame(self.canvased_frame)
+        self.buttons_down.pack(side='top', fill='both', expand=True)
         self.buttons_down.grid_rowconfigure(0, weight=1)
         self.buttons_down.grid_columnconfigure(0, weight=1)
         self.buttons_down.grid_columnconfigure(1, weight=1)
 
-        self.savebtn = ttk.Button(
-            self.buttons_down, text='Save', command=self.save)
+        self.savebtn = ttk.Button(self.buttons_down, text='Save', command=self.save)
         self.savebtn.grid(row=0, column=0, sticky=N+W+E+S)
 
-        self.closebtn = ttk.Button(
-            self.buttons_down, text='Close', command=self.close)
+        self.closebtn = ttk.Button(self.buttons_down, text='Close', command=self.close)
         self.closebtn.grid(row=0, column=1, sticky=N+E+W+S)
+
+        self.canvas.create_window(0, 0, anchor='nw', window=self.canvased_frame)
+        self.canvased_frame.update_idletasks()
+        self.canvas.config(scrollregion=self.canvas.bbox('all'))
+
 
     def save(self):
         if self.save_callback:
--- k0haku-notes/utils/helpers.py
+++ k0haku-notes/utils/helpers.py
@@ -1,8 +1,5 @@
 import tkinter as tk
 
-from frames.add_notes import AddNotesWindow, AddNotesWindowController
-from frames.start_page import StartPage, StartPageController
-
 
 class FrameHolder(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -18,3 +15,21 @@ class FrameHolder(tk.Frame):
         view = View(self)
         view.grid(row=0, column=0, sticky='nsew')
         self.views[View] = view
+
+
+class AutoScrollbar(tk.Scrollbar):
+    # A scrollbar that hides itself if it's not needed.
+    # Only works if you use the grid geometry manager!
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            # grid_remove is currently missing from Tkinter!
+            self.tk.call("grid", "remove", self)
+        else:
+            self.grid()
+        tk.Scrollbar.set(self, lo, hi)
+
+    def pack(self, **kw):
+        raise tk.TclError("cannot use pack with this widget")
+
+    def place(self, **kw):
+        raise tk.TclError("cannot use place with this widget")
